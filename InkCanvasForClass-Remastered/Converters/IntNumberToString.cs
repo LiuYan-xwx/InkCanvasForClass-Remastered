c4b7af3a0181922071d5b5268ee5b4bac491766d
LiuYan-xwx <LiuYan_xwx@outlook.com>
2025-07-29 16:20:49 +0800

    style: 将转换器们分开

---
+++ InkCanvasForClass-Remastered/Converters/IntNumberToString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace InkCanvasForClass_Remastered.Converters
+{
+    public class IntNumberToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((double)value == 0)
+            {
+                return "无限制";
+            }
+            else
+            {
+                return ((double)value).ToString() + "人";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((double)value == 0)
+            {
+                return "无限制";
+            }
+            else
+            {
+                return ((double)value).ToString() + "人";
+            }
+        }
+    }
+}
---
+++ InkCanvasForClass-Remastered/Converters/IntNumberToString2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace InkCanvasForClass_Remastered.Converters
+{
+    public class IntNumberToString2 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((double)value == 0)
+            {
+                return "自动截图";
+            }
+            else
+            {
+                return ((double)value).ToString() + "条";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((double)value == 0)
+            {
+                return "自动截图";
+            }
+            else
+            {
+                return ((double)value).ToString() + "条";
+            }
+        }
+    }
+}
