1165a675d2d0297b833b54f59c7a7d377a5dea42
LiuYan-xwx <LiuYan_xwx@outlook.com>
2025-08-30 16:06:27 +0800

    feat: 重构SettingsControl，支持添加描述文本和覆盖switcher

---
+++ InkCanvasForClass-Remastered/Controls/SettingsControl.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace InkCanvasForClass_Remastered.Controls
+{
+    public partial class SettingsControl : UserControl
+    {
+        public SettingsControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register(nameof(Header), typeof(string), typeof(SettingsControl), new PropertyMetadata(""));
+
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register(nameof(Description), typeof(string), typeof(SettingsControl), new PropertyMetadata(""));
+
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public static readonly DependencyProperty IsOnProperty =
+            DependencyProperty.Register(nameof(IsOn), typeof(bool), typeof(SettingsControl), new PropertyMetadata(false));
+
+        public bool IsOn
+        {
+            get => (bool)GetValue(IsOnProperty);
+            set => SetValue(IsOnProperty, value);
+        }
+
+        public static readonly DependencyProperty SwitcherProperty =
+            DependencyProperty.Register(nameof(Switcher), typeof(object), typeof(SettingsControl), new PropertyMetadata(null));
+
+        public object Switcher
+        {
+            get => GetValue(SwitcherProperty);
+            set => SetValue(SwitcherProperty, value);
+        }
+
+        public static readonly RoutedEvent ToggledEvent =
+            EventManager.RegisterRoutedEvent(nameof(Toggled), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(SettingsControl));
+
+        public event RoutedEventHandler Toggled
+        {
+            add => AddHandler(ToggledEvent, value);
+            remove => RemoveHandler(ToggledEvent, value);
+        }
+
+        private void ToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(ToggledEvent));
+        }
+    }
+}
