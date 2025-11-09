ed3bef3583d4ba118507b04839a5c4ffb7bc7d30
LiuYan-xwx <LiuYan_xwx@outlook.com>
2025-11-09 15:29:52 +0800

    feat: 浮动栏按钮控件

---
+++ InkCanvasForClass-Remastered/Controls/FloatingBarButton.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace InkCanvasForClass_Remastered.Controls
+{
+    /// <summary>
+    /// FloatingBarButton.xaml 的交互逻辑
+    /// </summary>
+    public partial class FloatingBarButton : UserControl
+    {
+        public FloatingBarButton()
+        {
+            InitializeComponent();
+            PART_Button.Click += (s, e) => RaiseEvent(new RoutedEventArgs(ClickEvent));
+        }
+
+        public static readonly DependencyProperty IconSourceProperty =
+            DependencyProperty.Register(
+                nameof(IconSource),
+                typeof(ImageSource),
+                typeof(FloatingBarButton),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(
+                nameof(Label),
+                typeof(string),
+                typeof(FloatingBarButton),
+                new PropertyMetadata(string.Empty));
+
+        public static readonly RoutedEvent ClickEvent =
+            EventManager.RegisterRoutedEvent(
+                nameof(Click),
+                RoutingStrategy.Bubble,
+                typeof(RoutedEventHandler),
+                typeof(FloatingBarButton));
+
+        public ImageSource IconSource
+        {
+            get => (ImageSource)GetValue(IconSourceProperty);
+            set => SetValue(IconSourceProperty, value);
+        }
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public event RoutedEventHandler Click
+        {
+            add => AddHandler(ClickEvent, value);
+            remove => RemoveHandler(ClickEvent, value);
+        }
+    }
+}
